449a9df7c6165844e66cdd9d5965a948ad16662d
NgocThach <62742480+onelove1816@users.noreply.github.com>
2021-02-26 09:31:43 +0700

    Update Lab2

---
+++ Lab2/Client/Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint serEndPoint = new IPEndPoint(IPAddress.Loopback, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Connecting to Server.........");
+
+
+            try
+            {
+                serverSocket.Connect(serEndPoint);
+
+
+                if (serverSocket.Connected)
+                {
+                    byte[] buff = new byte[100];
+
+                    int byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                    string str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                    Console.WriteLine(str);
+                    Console.WriteLine("Nhap du lieu gui len Server: \n");
+
+                    while (true)
+                    {
+                        str = Console.ReadLine(); buff = Encoding.ASCII.GetBytes(str);
+                        serverSocket.Send(buff, 0, buff.Length, SocketFlags.None); buff = new byte[1024];
+                        byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                        str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                        Console.WriteLine(str);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Khong the ket noi toi Server");
+            }
+
+        }
+    }
+}
---
+++ Lab2/Server/Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(serverEndPoint);
+            serverSocket.Listen(10);
+            Console.WriteLine("Waiting.........");
+            Socket clientSocket = serverSocket.Accept();
+            EndPoint clientEndPoint = clientSocket.RemoteEndPoint; 
+            Console.WriteLine("Client co dia chi va port: " + clientEndPoint.ToString() + " da ket noi toi");
+            byte[] buff;
+            string hello = "Hello Client";
+
+            buff = Encoding.ASCII.GetBytes(hello);
+
+            clientSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+            Console.WriteLine("Nhan du lieu tu client gui len! \n");
+            while (true)
+            {
+                buff = new byte[1024];
+                int byteReceive = clientSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                string str = Encoding.ASCII.GetString(buff, 0, byteReceive); Console.WriteLine(str);
+                clientSocket.Send(buff, 0, byteReceive, SocketFlags.None);
+            }
+
+          
+        }
+    }
+}
+
